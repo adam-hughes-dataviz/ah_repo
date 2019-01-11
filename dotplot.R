3673f4a80777d290d9fe527b1922eccf8373f0a1
adam-hughes-devinit <43996469+adam-hughes-devinit@users.noreply.github.com>
2019-01-11 10:49:43 +0000

    Add files via upload

--- dotplot.R
+++ dotplot.R
@@ -1,12 +1,19 @@
+#Load in ggplot
 library(ggplot2)
+
+#Read .csv
 landdata <- read.csv("landdata-states.csv")
+
+#define p1, adding x and y variables, a theme and a size for the non-data sections of the chart
 p1 <- ggplot(landdata,
              aes(x = State, 
                  y = Home.Price.Index)) +
   theme(legend.position = "top",
         axis.text = element_text(size = 6))
 
+#add the stylinh for the geom_points: alpha (transparency), size and position
 (p2 <- p1 + geom_point(aes(color = Date),
                       alpha = 0.5,
                       size = 1.5,
                       position = position_jitter(width = 0.25, height = 0)))
+#try deleting the position = position_jitter to discover its importance 
