2a8e73b89e13fa721906f4cf5351b9d2c12a7916
adam-hughes-devinit <43996469+adam-hughes-devinit@users.noreply.github.com>
2019-01-11 14:32:10 +0000

    Add files via upload

--- dotplot.R
+++ dotplot.R
@@ -1,3 +1,5 @@
+setwd("C:/git/Experiment/Rgraphics/dataSets")
+
 #Load in ggplot
 library(ggplot2)
 
@@ -12,8 +14,19 @@ p1 <- ggplot(landdata,
         axis.text = element_text(size = 6))
 
 #add the stylinh for the geom_points: alpha (transparency), size and position
-(p2 <- p1 + geom_point(aes(color = Date),
+p2 <- p1 + geom_point(aes(color = Date),
                       alpha = 0.5,
                       size = 1.5,
-                      position = position_jitter(width = 0.25, height = 0)))
+                      position = position_jitter(width = 0.25, height = 0))
 #try deleting the position = position_jitter to discover its importance 
+
+#Now we can change the x-axis name and scale the key in three discrete intervals while keeping the colours continuous
+p2 + scale_x_discrete(name="State Abbreviation") +
+  scale_color_continuous(name="",
+                         breaks = c(1976, 1994, 2013),
+                         labels = c("76","94","13"),
+                         low = "blue", high = "red")
+#We can use 'low' and 'high' colour values to customise the gradient of coloration. Remember to keep this continuous 
+
+
+
